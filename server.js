d37836e2436d4b8c5347611e3232ac636f9fb7e5
rodaan <rodaan.rabang@gmail.com>
2016-05-24 14:42:09 -0700

    Add chat functionality

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ io.on('connection', function(socket){
     });
   });
 
+  socket.on('chat message',function(msg){
+    io.emit('chat message', msg);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
